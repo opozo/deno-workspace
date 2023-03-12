cfe07dcca069552aa13c416db44835f1c243d793
Orlando Pozo <orlandopozo@gmail.com>
2023-03-11 20:57:01 -0800

    Delete test.ts

--- test.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
-
-function handler(_req: Request): Response {
-  return new Response("Hello Orlando!");
-}
-
-console.log("Listening on http://localhost:8000");
-serve(handler);
